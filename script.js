7b1a81163555bdac42d65e7eb82ba010114c57cc
Paul Li <paulkay19@gmail.com>
2025-06-09 23:41:53 -0700

    Select DOM elements for container, buttons, and input

---
+++ script.js
@@ -0,0 +1,8 @@
+const container = document.querySelector('.container');
+const btn = document.querySelector(".reset-btn");
+const erase = document.querySelector(".erase-btn");
+const inputNumber = document.querySelector(".input-number");
+const currentGridSquare = document.querySelector(".current-grid-square");
+const errorMessage = document.querySelector(".error-message");
+
+let initialValue;
