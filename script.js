c6c43e08c079e9d6eebcc38c6f95735379c2a06e
Paul Li <paulkay19@gmail.com>
2025-06-09 23:53:10 -0700

    Implement grid square creation based on grid size

--- script.js
+++ script.js
@@ -6,3 +6,13 @@ const currentGridSquare = document.querySelector(".current-grid-square");
 const errorMessage = document.querySelector(".error-message");
 
 let initialValue;
+
+function createSquare(gridSize) {
+  for (let i = 0; i < gridSize * gridSize; i++) {
+    const square = document.createElement("div");
+    square.classList.add("square");
+    square.style.width = (550 / gridSize) + "px";
+    square.style.height = (550 / gridSize) + "px";
+    container.appendChild(square);
+  }
+}
